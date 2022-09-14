e6fc1319a98ea2ec9fdcf3b1484f37693c991087
Boris Milanovic <103489050+cyber-milanovicb@users.noreply.github.com>
2022-09-14 09:21:53 +0200

    Dodati komentari
    www.besplatnikursevi.iiiii.info - Boris Milanovic, CERTIFIED SYSTEM NETWORK ADMINISTRATOR

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,13 +1,13 @@
 import sys
 
-x = int
-x = input("What's your favorite number?")
+x = int #definisemo x kao tip promenjive INTEGER
+x = input("Koji je Vaš omiljeni broj?") # definisemo varijablu/ promenjivu X tako da bude ono sto je input (odgovor/ulaz) za pitanje "What's your favorite number?"
 
 
 if x == 100:
-    print("The favorite number is 100")
+    print("Omiljeni broj je 100")
 else:
     while x > str(100) or x < str(100):
-        print("Your favorite number is " + x)
+        print("Tvoj omiljeni broj je " + x)
 
     sys.exit()
